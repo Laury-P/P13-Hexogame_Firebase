910717b3d50a09de8608fea748b90fc17de562d5
Laury PRIN <prinlauryy@gmail.com>
2026-04-13 15:41:30 +0200

    Notification s'affiche que l'app soit fermé ou non donc reception OK, affichage OK

--- app/src/main/java/com/openclassrooms/hexagonal/games/notification/NotificationService.kt
+++ app/src/main/java/com/openclassrooms/hexagonal/games/notification/NotificationService.kt
@@ -1,7 +1,9 @@
 package com.openclassrooms.hexagonal.games.notification
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.util.Log
-import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -21,6 +23,24 @@ class NotificationService : FirebaseMessagingService() {
     }
 
     private fun showNotification(title: String?, body: String?) {
-        Log.d("FCM", "Notification received: $title, $body")
+        val channelId = "default"
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId, "Principal", NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+            .build()
+
+        // id = 0 -> Each notification will erase the previous one.
+        // or id = System.currentTimeMillis().toInt() -> Each notification will be unique and stack on top of each other.
+        notificationManager.notify(System.currentTimeMillis().toInt(), notification)
     }
 }
