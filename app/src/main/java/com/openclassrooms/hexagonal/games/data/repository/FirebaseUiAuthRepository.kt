1daf7b056875d154a049a77e7662d0186fe93567
Laury PRIN <prinlauryy@gmail.com>
2026-04-14 11:52:50 +0200

    Decouplage de login

--- app/src/main/java/com/openclassrooms/hexagonal/games/data/repository/FirebaseUiAuthRepository.kt
+++ app/src/main/java/com/openclassrooms/hexagonal/games/data/repository/FirebaseUiAuthRepository.kt
@@ -3,6 +3,7 @@ package com.openclassrooms.hexagonal.games.data.repository
 import android.content.Context
 import com.firebase.ui.auth.AuthState
 import com.firebase.ui.auth.FirebaseAuthUI
+import com.openclassrooms.hexagonal.games.domain.model.LocalAuthState
 import com.openclassrooms.hexagonal.games.domain.model.User
 import com.openclassrooms.hexagonal.games.domain.repository.AuthRepository
 import kotlinx.coroutines.flow.Flow
@@ -10,8 +11,21 @@ import kotlinx.coroutines.flow.Flow
 
 class FirebaseUiAuthRepository : AuthRepository {
 
-    override fun userLogState(): Flow<AuthState> {
-        return FirebaseAuthUI.getInstance().authStateFlow()
+    override fun userLogState(): Flow<LocalAuthState> {
+        return FirebaseAuthUI.getInstance()
+            .authStateFlow()
+            .map { firebaseAuthState ->
+                when (firebaseAuthState) {
+                    is AuthState.Success -> {
+                        LocalAuthState.LoggedIn(firebaseAuthState.user.uid)
+                    }
+                    is AuthState.RequiresEmailVerification -> {
+                        LocalAuthState.LoggedIn(firebaseAuthState.user.uid)
+                    }
+                    is AuthState.Loading -> LocalAuthState.Loading
+                    else -> LocalAuthState.LoggedOut
+                }
+            }
     }
 
     override suspend fun getCurrentUser(): User? {
--- app/src/main/java/com/openclassrooms/hexagonal/games/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/openclassrooms/hexagonal/games/domain/repository/AuthRepository.kt
@@ -1,13 +1,11 @@
 package com.openclassrooms.hexagonal.games.domain.repository
 
-import android.content.Context
-import android.content.Intent
-import com.firebase.ui.auth.AuthState
+import com.openclassrooms.hexagonal.games.domain.model.LocalAuthState
 import com.openclassrooms.hexagonal.games.domain.model.User
 import kotlinx.coroutines.flow.Flow
 
 interface AuthRepository {
-    fun userLogState(): Flow<AuthState>
+    fun userLogState(): Flow<LocalAuthState>
     suspend fun getCurrentUser(): User?
     suspend fun signOut(context: Context)
     suspend fun deleteAccount(context: Context)
