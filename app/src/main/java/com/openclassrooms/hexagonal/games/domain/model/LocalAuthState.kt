571aa54ab421b1c04f0a17ba2fedf9e8e9cedc0c
Laury PRIN <prinlauryy@gmail.com>
2026-04-23 16:18:48 +0200

    Correction LocalAuthState.kt

--- app/src/main/java/com/openclassrooms/hexagonal/games/domain/model/LocalAuthState.kt
+++ app/src/main/java/com/openclassrooms/hexagonal/games/domain/model/LocalAuthState.kt
@@ -2,6 +2,6 @@ package com.openclassrooms.hexagonal.games.domain.model
 
 sealed class LocalAuthState {
     object Loading : LocalAuthState()
-    object LoggedIn : LocalAuthState()
+    data class LoggedIn (val uid: String): LocalAuthState()
     object LoggedOut : LocalAuthState()
 }
