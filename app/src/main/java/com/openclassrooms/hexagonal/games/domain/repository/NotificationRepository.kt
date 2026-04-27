98e5d4548c0252ed2b010d3f8bac0eda43eeb7a4
Laury PRIN <prinlauryy@gmail.com>
2026-04-27 17:16:13 +0200

    Mise a jours du viewmodel et du screen pour gerer l'abonnement ou non au notification

--- app/src/main/java/com/openclassrooms/hexagonal/games/domain/repository/NotificationRepository.kt
+++ app/src/main/java/com/openclassrooms/hexagonal/games/domain/repository/NotificationRepository.kt
@@ -1,8 +1,8 @@
 package com.openclassrooms.hexagonal.games.domain.repository
 
 interface NotificationRepository {
-    suspend fun subscribeToAllNotification()
-    suspend fun unsubscribeFromAllNotification()
+    suspend fun subscribeToAllNotification() : Result<Unit>
+    suspend fun unsubscribeFromAllNotification() : Result<Unit>
 }
 
 
